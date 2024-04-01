381fecc2b1f5462b0d02c904648ddfff256ba195
ksj0813 <tow369@naver.com>
2024-04-01 15:31:30 +0900

    Exam

---
+++ src/main/java/Exam.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+//3의 배수?
+//5의 배수?
+//3의 배수와 5의 배수를 모두 만족하는지?
+public class Exam {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int a ;
+        a = s.nextInt();
+
+            if( (a%3 == 0) && (a%5 ==0) )
+                System.out.print(" 3의배수,5의배수 모두 만족합니다.");
+            else if ( a%3 == 0 )
+                System.out.print(" 3의 배수입니다.");
+            else if ( a%5 == 0 )
+                System.out.print(" 5의 배수입니다.");
+
+    }
+}
