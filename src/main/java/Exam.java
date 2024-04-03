2112f7294066b47114e5fe6b21e98e516161cf59
ksj0813 <tow369@naver.com>
2024-04-03 20:34:26 +0900

    Exam

--- src/main/java/Exam.java
+++ src/main/java/Exam.java
@@ -7,15 +7,16 @@ public class Exam {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        int a ;
+        int a;
         a = s.nextInt();
 
-            if( (a%3 == 0) && (a%5 ==0) )
-                System.out.print(" 3의배수,5의배수 모두 만족합니다.");
-            else if ( a%3 == 0 )
-                System.out.print(" 3의 배수입니다.");
-            else if ( a%5 == 0 )
-                System.out.print(" 5의 배수입니다.");
+        if ((a % 3 == 0) && (a % 5 == 0)) { // if(num%15 == 0)
+            System.out.print(" 3의배수,5의배수 모두 만족합니다.");
+        } else if (a % 3 == 0) {
+            System.out.print(" 3의 배수입니다.");
+        } else if (a % 5 == 0) {
+            System.out.print(" 5의 배수입니다.");
 
+        }
     }
 }
