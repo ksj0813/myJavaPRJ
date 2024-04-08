42dfe639cc2c57db71aea5cf305efb94717626ae
ksj0813 <tow369@naver.com>
2024-04-08 17:27:16 +0900

    Term3_1

---
+++ src/main/java/sujin/Term3.java
@@ -0,0 +1,19 @@
+package sujin;
+
+import java.util.Scanner;
+
+public class Term3 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+
+        if(a>b) {
+            System.out.println("a는 b보다 큽니다.");
+        }else if(a==b) {
+            System.out.println("a는 b와 같습니다.");
+        }else if(a<b) {
+            System.out.println("a는 b보다 작습니다.");
+        }
+    }
+}
