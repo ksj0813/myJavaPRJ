da21ee8bb2338aec2ab6a2396c6f5d5c5c54c45a
ksj0813 <tow369@naver.com>
2024-08-05 16:55:18 +0900

    한행씩 읽기

---
+++ src/main/java/JAVA/JAVA/Code11_01.java
@@ -0,0 +1,25 @@
+package JAVA.JAVA;
+
+import java.io.BufferedReader;
+
+import java.io.FileReader;
+
+public class Code11_01 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new FileReader("c:/FirstJava/myData1.txt"));
+        String inStr;
+
+        inStr = br.readLine();
+        System.out.println(inStr);
+
+        inStr = br.readLine();
+        System.out.println(inStr);
+
+        inStr = br.readLine();
+        System.out.println(inStr);
+
+        br.close();
+
+
+    }
+}
